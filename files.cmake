94e1c99c03d116e2a12d3c95e3e84289b6513972
Nikolay Zapolnov <zapolnov@gmail.com>
2014-01-30 00:43:03 +0400

    Added normal GL wrappers.

--- files.cmake
+++ files.cmake
@@ -11,6 +11,14 @@ SET(dummy_gl_wrappers_files
 	dummy/dummy_gl.cpp
 )
 
+SET(normal_gl_wrappers_files
+	glew/include/GL/glew.h
+	glew/include/GL/glxew.h
+	glew/include/GL/wglew.h
+	glew/src/glew.c.h
+	normal/normal_gl.cpp
+)
+
 SET(qt_gl_wrappers_files
 	qt/qt_gl.cpp
 	qt/qt_gl.h
